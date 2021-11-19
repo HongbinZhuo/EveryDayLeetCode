1778da17be59e9064cb1ba6660301b8f9c121e70
zhuohongbin <zhuohongbin@apexsoft.com.cn>
2021-11-19 15:12:58 +0800

    旧代码合并233

---
+++ StudySpace/JAVA8/src/main/java/streams/StreamsOne.java
@@ -0,0 +1,49 @@
+package streams;
+
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/**
+ * @Author Zhuohongbin
+ * @Description TODO
+ * @Date 2021/1/12 16:33
+ */
+public class StreamsOne {
+    public static void main(String[] args) {
+        Clock clock = Clock.systemUTC();
+        System.out.println(clock.instant());
+        System.out.println(clock.millis());
+        /////////////////
+        final LocalDateTime datetime = LocalDateTime.now();
+        final LocalDateTime datetimeFromClock = LocalDateTime.now(clock);
+        System.out.println("***************************");
+        System.out.println(datetime );
+        System.out.println(datetimeFromClock );
+        /////////////////
+
+
+        System.out.println("***************************");
+// Get the local date and local time
+        final LocalDate date = LocalDate.now();
+        final LocalDate dateFromClock = LocalDate.now( clock );
+
+        System.out.println( date );
+        System.out.println( dateFromClock );
+        System.out.println("***************************");
+// Get the local date and local time
+        final LocalTime time = LocalTime.now();
+        final LocalTime timeFromClock = LocalTime.now( clock );
+
+        System.out.println( time );
+        System.out.println( timeFromClock );
+
+
+    }
+
+
+
+
+}
